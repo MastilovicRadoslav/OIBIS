4a348e534e432b22dce1286e56c0814397a569cd
DajanaRadovic <dajana.radovic19@gmail.com>
2023-10-18 19:33:39 +0200

    Menu

--- Project_3/Client/Program.cs
+++ Project_3/Client/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Client
 {
-	internal class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
-		}
+            while (true)
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Choose an option :");
+                Console.WriteLine("\t1. Read");
+                Console.WriteLine("\t2. Read, Calculate");
+                Console.WriteLine("\t3. Read, Modify");
+                Console.WriteLine("\t3. Read, Administer");
+                Console.WriteLine("\t0. Exit the application!");
+                string option = Console.ReadLine();
+                Console.WriteLine("--------------------------------");
+
+                switch (option)
+                {
+                    case "1": 
+                    {
+                            //ReadDataFromDatabase();
+                            Console.WriteLine("Reading data...");
+                        break;
+                    }
+                    case "2":
+                    {
+                            //ReadDataFromDatabase();
+                            //CalculateConsumption();
+                            Console.WriteLine("Reading data...");
+                            Console.WriteLine("Result:...");
+                            
+                            break;
+                    }
+                    case "3":
+                    {
+                        break;
+                    }
+                    case "4":
+                    {
+                        break;
+                    }
+                    case "0":
+                        {
+                            Console.WriteLine("Existing the application.");
+                            return;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid option!");
+                            break;
+                        }
+
+                }
+            }
+        }
 	}
 }
--- Project_3/Common/Measurement.cs
+++ Project_3/Common/Measurement.cs
@@ -9,20 +9,20 @@ namespace Common
     public class Measurement
     {
         private int id; // Jedinstveni identifikator
-        private string? region; // Region
-        private string? city; // Grad
-        private string? year; // Godina
-        private string? consumption; // Potrošnja električne energije za mesece
+        private string region; // Region
+        private string city; // Grad
+        private string year; // Godina
+        private string consumption; // Potrošnja električne energije za mesece
 
         // Svojstva (Properties)
         public int Id { get => id; set => id = value; }
-        public string? Region { get => region; set => region = value; }
-        public string? City { get => city; set => city = value; }
-        public string? Year { get => year; set => year = value; }
-        public string? Consumption { get => consumption; set => consumption = value; }
+        public string Region { get => region; set => region = value; }
+        public string City { get => city; set => city = value; }
+        public string Year { get => year; set => year = value; }
+        public string Consumption { get => consumption; set => consumption = value; }
 
         // Funkcija za ispis
-        public override string? ToString() => $"Id = {Id}, Region = {Region}, City = {City}, Year = {Year}, Consumption = {Consumption}" + "\n";
+        public override string ToString() => $"Id = {Id}, Region = {Region}, City = {City}, Year = {Year}, Consumption = {Consumption}" + "\n";
 
         // Konstruktor bez parametara
         public Measurement()
@@ -30,7 +30,7 @@ namespace Common
         }
 
         // Konstruktor sa parametrima
-        public Measurement(int id, string? region, string? city, string? year, string? consumption)
+        public Measurement(int id, string region, string city, string year, string consumption)
         {
             Id = id;
             Region = region;
