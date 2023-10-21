4023013e894b698f163887f4d717a8e03523eac1
DajanaRadovic <dajana.radovic19@gmail.com>
2023-10-21 15:19:55 +0200

    Connection class

---
+++ Project_3/LocalDatabase/Connection.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDatabase
+{
+    public class Connection : IConnection
+    {
+        public List<Measurement> specificRegionList = new List<Measurement>();
+        public DataBase db = new DataBase();
+        List<Measurement> IConnection.PrintMeasurements()
+        {
+            specificRegionList = db.ReadMeasurementsFromFile("measuredDataForRegion.txt");
+            return specificRegionList;
+        }
+    }
+}
