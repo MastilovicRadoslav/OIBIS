23106fdb33070dc2d4610997dc56f1a6d3ced1b8
DajanaRadovic <dajana.radovic19@gmail.com>
2023-10-21 15:15:12 +0200

    Menu-change

--- Project_3/Client/Program.cs
+++ Project_3/Client/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace Client
 {
@@ -6,46 +9,55 @@ namespace Client
 	{
 		static void Main(string[] args)
 		{
-			while (true)
-			{
-				Console.WriteLine("-------------------------------");
-				Console.WriteLine("Choose an option :");
-				Console.WriteLine("\t1. Read");
-				Console.WriteLine("\t2. Read, Calculate");
-				Console.WriteLine("\t3. Read, Modify");
-				Console.WriteLine("\t4. Read, Administer");
-				Console.WriteLine("\t0. Exit the application!");
-				string option = Console.ReadLine();
-				Console.WriteLine("--------------------------------");
+            ChannelFactory<IConnection> channel = new ChannelFactory<IConnection>("ServiceName");
+            IConnection proxy = channel.CreateChannel();
 
-				switch (option)
-				{
-					case "1":
-						//ReadDataFromDatabase();
-						Console.WriteLine("Reading data...");
-						break;
+            while (true)
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine("Choose an option :");
+                Console.WriteLine("\t1. Read");
+                Console.WriteLine("\t2. Read, Calculate");
+                Console.WriteLine("\t3. Read, Modify");
+                Console.WriteLine("\t4. Read, Administer");
+                Console.WriteLine("\t0. Exit the application!");
+                string option = Console.ReadLine();
+                Console.WriteLine("--------------------------------");
 
-					case "2":
-						//ReadDataFromDatabase();
-						//CalculateConsumption();
-						Console.WriteLine("Reading data...");
-						Console.WriteLine("Result:...");
-						break;
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("\nPrint data from the local database :\n");
+                        List<Measurement> listToPrint = proxy.PrintMeasurements();
 
-					case "3":
-						break;
+                        foreach (var item in listToPrint)
+                        {
+                            Console.Write(item.ToString());
+                        }
+                        Console.WriteLine();
+                        break;
 
-					case "4":
-						break;
+                    case "2":
+                        //ReadDataFromDatabase();
+                        //CalculateConsumption();
+                        Console.WriteLine("Reading data...");
+                        Console.WriteLine("Result:...");
+                        break;
 
-					case "0":
-						return;
+                    case "3":
+                        break;
 
-					default:
-						Console.WriteLine("Invalid option!");
-						break;
-				}
-			}
-		}
+                    case "4":
+                        break;
+
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid option!");
+                        break;
+                }
+            }
+        }
 	}
 }
