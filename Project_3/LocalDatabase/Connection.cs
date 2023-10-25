7ed0d6686ac7782a5c0e5893b4d6220a522b87db
DajanaRadovic <dajana.radovic19@gmail.com>
2023-10-25 15:22:27 +0200

    Modify

--- Project_3/LocalDatabase/Connection.cs
+++ Project_3/LocalDatabase/Connection.cs
@@ -16,5 +16,48 @@ namespace LocalDatabase
             specificRegionList = db.ReadMeasurementsFromFile("measuredDataForRegion.txt");
             return specificRegionList;
         }
+
+        public bool Modify(string id, string value)
+        {
+            specificRegionList = db.ReadMeasurementsFromFile("measuredDataForRegion.txt");
+            int localId = int.Parse(id);
+            DateTime currentTime = DateTime.Now;
+            string currentMonth = currentTime.ToString("MMMM");
+            int counter = 0;
+
+            for (int iIndex = 0; iIndex < specificRegionList.Count; iIndex++)
+            {
+                var i = specificRegionList[iIndex];
+
+                if (i.Id == localId)
+                {
+                    var consumption = i.Consumption;
+                    for (int cIndex = 0; cIndex < consumption.Count; cIndex++)
+                    {
+                        var c = consumption.ElementAt(cIndex);
+                        if (c.Key.Equals(currentMonth))
+                        {
+                            consumption[c.Key] = value;
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+            // Otvorite datoteku za pisanje i odmah je zatvorite, čime ćete je očistiti.
+            File.WriteAllText("measuredDataForRegion.txt", string.Empty);
+
+            foreach (var i in specificRegionList)
+            {
+                db.WriteMeasurementToFile(i, "measuredDataForRegion.txt");
+            }
+
+
+            if (counter == 0)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
