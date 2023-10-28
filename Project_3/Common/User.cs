59df6b4cfff74284919db53c3e63c1464e2471cd
vanjamihajlovic <mihajlovicavanja@gmail.com>
2023-10-28 18:28:31 +0200

    User

---
+++ Project_3/Common/User.cs
@@ -0,0 +1,27 @@
+﻿namespace Common
+{
+    public class User
+    {
+        string username = string.Empty;
+        string password = string.Empty;
+
+        public User(string _username, string _password)
+        {
+            this.username = _username;
+            this.password = _password;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+    }
+}
