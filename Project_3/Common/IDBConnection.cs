aa7bc9c9757d580d7e3a70c273ff1ff3c8f120a8
vanjamihajlovic <mihajlovicavanja@gmail.com>
2023-11-08 16:08:52 +0100

    IDBConnection

--- Project_3/Common/IDBConnection.cs
+++ Project_3/Common/IDBConnection.cs
@@ -3,10 +3,19 @@ using System.ServiceModel;
 
 namespace Common
 {
-	[ServiceContract]
-	public interface IDBConnection
-	{
-		[OperationContract]
-		List<Measurement> DataTransfer();
-	}
+    [ServiceContract]
+    public interface IDBConnection
+    {
+        [OperationContract]
+        List<Measurement> DataTransfer();
+
+        [OperationContract]
+        void UpdatingCentralDataBase(List<Measurement> updatedList);
+
+        [OperationContract]
+        void AddNewEntityToCentralDB(List<Measurement> listOfNewEntities);
+
+        [OperationContract]
+        void DeleteEntityFromCentralDB(List<Measurement> listWithDeletedEntities);
+    }
 }
