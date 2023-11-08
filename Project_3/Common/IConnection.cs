4b807c9368e3848c0eab62e651c824b90a5a7b28
DajanaRadovic <dajana.radovic19@gmail.com>
2023-11-08 16:07:18 +0100

    IConnection-changes

--- Project_3/Common/IConnection.cs
+++ Project_3/Common/IConnection.cs
@@ -4,28 +4,37 @@ using System.ServiceModel;
 
 namespace Common
 {
-	[ServiceContract]
-	public interface IConnection
-	{
-		[OperationContract]
-		List<Measurement> PrintMeasurements();
+    [ServiceContract]
+    public interface IConnection
+    {
+        [OperationContract]
+        List<Measurement> PrintMeasurements();
 
-		[OperationContract]
-		Double CalculateConsumptionMeanCity(string city);
+        [OperationContract]
+        Double CalculateConsumptionMeanCity(string city);
 
-		[OperationContract]
-		Double CalculateConsumptionMeanRegion(string region);
+        [OperationContract]
+        Double CalculateConsumptionMeanRegion(string region);
 
-		[OperationContract]
-		bool Modify(string id, string value);
+        [OperationContract]
+        bool Modify(string id, string value);
 
-		[OperationContract]
-		bool DeleteEntity(int idForDelete);
+        [OperationContract]
+        void ChangeInDB(bool change);
 
-		[OperationContract]
-		void AddUser(string username, string password);
+        [OperationContract]
+        bool AddNewEntity(Measurement newEntity);
 
-		[OperationContract]
-		bool AddNewEntity(Measurement newEntity);
-	}
+        [OperationContract]
+        void NewEntitiesInDB(bool change, int id);
+
+        [OperationContract]
+        bool DeleteEntity(int idForDelete);
+
+        [OperationContract]
+        void DeletedEntitiesInDB(bool change);
+
+        [OperationContract]
+        void AddUser(string username, string password);
+    }
 }
