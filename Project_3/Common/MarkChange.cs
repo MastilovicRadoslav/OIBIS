860d17cac6036ef8c5bd7e3a5de2a3e49429f966
Tojzán Krisztián <kristiantojzan@gmail.com>
2023-11-08 15:54:48 +0100

    Class for database changes

---
+++ Project_3/Common/MarkChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Common
+{
+    public class MarkChange
+    {
+        public List<Measurement> dataToModify;
+        public List<Measurement> listWithNewEntities;
+        public List<Measurement> listWithDeletedEntities;
+        public bool newEntitiesAdded = false;
+        public bool entitiesDeleted = false;
+        public bool dataChanged = false;
+    }
+}
