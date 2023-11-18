c0f82e00540295bd9b48fd1de478e22ffcbda29d
vanjamihajlovic <mihajlovicavanja@gmail.com>
2023-11-18 14:33:38 +0100

    SecurityManager

---
+++ Project_3/SecurityManager/CustomAuthorizationPolicy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Claims;
+using System.IdentityModel.Policy;
+using System.Security.Principal;
+
+namespace SecurityManager
+{
+    public class CustomAuthorizationPolicy : IAuthorizationPolicy
+    {
+        public CustomAuthorizationPolicy()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public ClaimSet Issuer
+        {
+            get { return ClaimSet.System; }
+        }
+        public string Id
+        {
+            get;
+        }
+
+        public bool Evaluate(EvaluationContext evaluationContext, ref object state)
+        {
+            if (!evaluationContext.Properties.TryGetValue("Identities", out object list))
+            {
+                return false;
+            }
+
+            IList<IIdentity> identities = list as IList<IIdentity>;
+            if (list == null || identities.Count <= 0)
+            {
+                return false;
+            }
+
+            evaluationContext.Properties["Principal"] =
+                new CustomPrincipal((WindowsIdentity)identities[0]);
+            return true;
+        }
+    }
+}
---
+++ Project_3/SecurityManager/CustomPrincipal.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Security.Principal;
+
+namespace SecurityManager
+{
+    class CustomPrincipal : IPrincipal
+    {
+        WindowsIdentity identity = null;
+        public CustomPrincipal(WindowsIdentity windowsIdentity)
+        {
+            identity = windowsIdentity;
+        }
+
+        public IIdentity Identity
+        {
+            get { return identity; }
+        }
+
+        public bool IsInRole(string permission)
+        {
+            foreach (IdentityReference group in this.identity.Groups)
+            {
+                SecurityIdentifier sid = (SecurityIdentifier)group.Translate(typeof(SecurityIdentifier));
+                var name = sid.Translate(typeof(NTAccount));
+                string groupName = Formatter.ParseName(name.ToString());
+                string[] permissions;
+                if (RolesConfig.GetPermissions(groupName, out permissions) && permissions.Contains(permission))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
