c2d3fe7db5affd5daad8c6c08756dcf4df698925
MastilovicRadoslav <radoslav930@gmail.com>
2023-12-02 16:45:29 +0100

    CentralDatabase - Program.cs

--- Project_3/CentralDatabase/Program.cs
+++ Project_3/CentralDatabase/Program.cs
@@ -9,9 +9,9 @@ namespace CentralDatabase
 	{
 		static void Main(string[] args)
 		{
-			// Formiranje TEST podataka
 			DataBase db = new DataBase();
 
+			#region Formiranje TEST podataka
 			Measurement m1 = new Measurement
 			{
 				Id = 1,
@@ -30,7 +30,7 @@ namespace CentralDatabase
 					{ "August", "123.09" },
 					{ "September", "155.19" },
 					{ "October", "233.85" },
-					{ "November", "0" },
+					{ "November", "39.09" },
 					{ "December", "0" }
 				}
 			};
@@ -53,7 +53,7 @@ namespace CentralDatabase
 					{ "August", "133.46" },
 					{ "September", "160.75" },
 					{ "October", "185.21" },
-					{ "November", "0" },
+					{ "November", "243.15" },
 					{ "December", "0" }
 				}
 			};
@@ -76,11 +76,10 @@ namespace CentralDatabase
 					{ "August", "122.46" },
 					{ "September", "162.75" },
 					{ "October", "215.21" },
-					{ "November", "0" },
+					{ "November", "53.45" },
 					{ "December", "0" }
 				}
 			};
-
 
 			Measurement m4 = new Measurement
 			{
@@ -100,7 +99,7 @@ namespace CentralDatabase
 					{ "August", "120.46" },
 					{ "September", "160.75" },
 					{ "October", "228.21" },
-					{ "November", "0" },
+					{ "November", "83.65" },
 					{ "December", "0" }
 				}
 			};
@@ -123,11 +122,10 @@ namespace CentralDatabase
 					{ "August", "123.09" },
 					{ "September", "157.19" },
 					{ "October", "230.85" },
-					{ "November", "0" },
+					{ "November", "163.91" },
 					{ "December", "0" }
 				}
 			};
-
 
 			Measurement m6 = new Measurement
 			{
@@ -147,16 +145,15 @@ namespace CentralDatabase
 					{ "August", "125.09" },
 					{ "September", "159.19" },
 					{ "October", "235.85" },
-					{ "November", "0" },
+					{ "November", "101.65" },
 					{ "December", "0" }
 				}
 			};
 
-
 			Measurement m7 = new Measurement
 			{
 				Id = 7,
-				Region = "Yapadni region",
+				Region = "Zapadni region",
 				City = "Novi Pazar",
 				Year = "2023",
 				Consumption = new Dictionary<string, string>
@@ -171,7 +168,7 @@ namespace CentralDatabase
 					{ "August", "126.09" },
 					{ "September", "156.19" },
 					{ "October", "233.85" },
-					{ "November", "0" },
+					{ "November", "267.45" },
 					{ "December", "0" }
 				}
 			};
@@ -194,11 +191,10 @@ namespace CentralDatabase
 					{ "August", "128.09" },
 					{ "September", "158.19" },
 					{ "October", "230.85" },
-					{ "November", "0" },
+					{ "November", "131.85" },
 					{ "December", "0" }
 				}
 			};
-
 
 			Measurement m9 = new Measurement
 			{
@@ -218,7 +214,7 @@ namespace CentralDatabase
 					{ "August", "122.09" },
 					{ "September", "154.19" },
 					{ "October", "228.85" },
-					{ "November", "0" },
+					{ "November", "423.35" },
 					{ "December", "0" }
 				}
 			};
@@ -241,24 +237,72 @@ namespace CentralDatabase
 					{ "August", "124.09" },
 					{ "September", "156.19" },
 					{ "October", "234.85" },
-					{ "November", "0" },
+					{ "November", "333.33" },
 					{ "December", "0" }
 				}
 			};
 
-			List<Measurement> list = new List<Measurement> { m1, m2, m3, m4, m5, m6, m7, m8, m9, m10 };
+			Measurement m11 = new Measurement
+			{
+				Id = 11,
+				Region = "Centralni region",
+				City = "Kragujevac",
+				Year = "2023",
+				Consumption = new Dictionary<string, string>
+				{
+					{ "January", "58.33" },
+					{ "February", "108.88" },
+					{ "March", "79.79" },
+					{ "April", "91.13" },
+					{ "May", "55.55" },
+					{ "June", "69.66" },
+					{ "July", "148.22" },
+					{ "August", "193.09" },
+					{ "September", "256.19" },
+					{ "October", "400.85" },
+					{ "November", "236.95" },
+					{ "December", "0" }
+				}
+			};
 
+			Measurement m12 = new Measurement
+			{
+				Id = 12,
+				Region = "Istočni region",
+				City = "Niš",
+				Year = "2023",
+				Consumption = new Dictionary<string, string>
+				{
+					{ "January", "59.33" },
+					{ "February", "21.88" },
+					{ "March", "71.29" },
+					{ "April", "84.13" },
+					{ "May", "41.55" },
+					{ "June", "98.66" },
+					{ "July", "142.82" },
+					{ "August", "159.09" },
+					{ "September", "316.19" },
+					{ "October", "281.15" },
+					{ "November", "113.13" },
+					{ "December", "0" }
+				}
+			};
+
+			List<Measurement> list = new List<Measurement> { m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12 };
+			#endregion
+
+			// Upis podataka u txt fajl, koji simulira bazu podataka
 			foreach (var x in list)
 			{
 				db.WriteMeasurementToFile(x, "measuredData.txt");
 			}
 
-			// Redovan deo - konekcija central i lokal DB
+			// Konekcija CentralDB i LocalDB
 			ServiceHost host = new ServiceHost(typeof(DBConnection));
 			try
 			{
 				host.Open();
-				Console.WriteLine("Server has started!!!!\n");
+				Console.WriteLine("Server has started !!!\n");
 				Console.ReadKey();
 			}
 			finally
