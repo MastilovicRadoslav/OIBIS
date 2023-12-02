e0b9dba9a9fc6955e1a89144c3480d8678f13de8
MastilovicRadoslav <radoslav930@gmail.com>
2023-12-02 16:47:05 +0100

    CentralDatabase - DBConnection.cs

--- Project_3/CentralDatabase/DBConnection.cs
+++ Project_3/CentralDatabase/DBConnection.cs
@@ -1,22 +1,28 @@
 ﻿using Common;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace CentralDatabase
 {
 	public class DBConnection : IDBConnection
 	{
 		public DataBase data = new DataBase();
+
+		// Funkcija za prenos podataka između CentralDB i LocalDB
 		public List<Measurement> DataTransfer()
 		{
 			List<Measurement> returnList = new List<Measurement>();
 			returnList = data.ReadMeasurementsFromFile("measuredData.txt");
+			returnList = returnList.OrderBy(entity => entity.Id).ToList();
 			return returnList;
 		}
 
+		// Ažuriranje CentralDB, ako je Client pozvao funkciju Modify
 		public void UpdatingCentralDataBase(List<Measurement> updatedList)
 		{
 			File.WriteAllText("measuredData.txt", string.Empty);
+			updatedList = updatedList.OrderBy(entity => entity.Id).ToList();
 
 			foreach (var x in updatedList)
 			{
@@ -24,9 +30,11 @@ namespace CentralDatabase
 			}
 		}
 
+		// Ažuriranje CentralDB, ako je dodat novi entitet u LocalDB
 		public void AddNewEntityToCentralDB(List<Measurement> listOfNewEntities)
 		{
 			File.WriteAllText("measuredData.txt", string.Empty);
+			listOfNewEntities = listOfNewEntities.OrderBy(entity => entity.Id).ToList();
 
 			foreach (var x in listOfNewEntities)
 			{
@@ -34,9 +42,11 @@ namespace CentralDatabase
 			}
 		}
 
+		// Ažuriranje CentralDB, ako je obrisan entitet iz LocalDB
 		public void DeleteEntityFromCentralDB(List<Measurement> listWithDeletedEntities)
 		{
 			File.WriteAllText("measuredData.txt", string.Empty);
+			listWithDeletedEntities = listWithDeletedEntities.OrderBy(entity => entity.Id).ToList();
 
 			foreach (var x in listWithDeletedEntities)
 			{
