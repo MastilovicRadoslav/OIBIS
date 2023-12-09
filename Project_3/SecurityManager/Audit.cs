3235ae6d43b480238b8ab5e1901e4cc8373ef9f5
vanjamihajlovic <mihajlovicavanja@gmail.com>
2023-12-09 15:16:45 +0100

    Audit, AuditEvents

---
+++ Project_3/SecurityManager/Audit.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Manager
+{
+    public class Audit : IDisposable
+    {
+        private static EventLog customLog = null;
+        const string SourceName = "Manager.Audit";
+        const string LogName = "CentralDBLogs";
+
+        static Audit()
+        {
+            try
+            {
+                if (!EventLog.SourceExists(SourceName))
+                {
+                    EventLog.CreateEventSource(SourceName, LogName);
+                }
+                customLog = new EventLog(LogName,
+                    Environment.MachineName, SourceName);
+            }
+            catch (Exception e)
+            {
+                customLog = null;
+                Console.WriteLine("Error while trying to create log handle. Error = {0}", e.Message);
+            }
+        }
+
+
+        public static void Add()
+        {
+            if (customLog != null)
+            {
+                string UserAddingSuccess = AuditEvents.Add;
+                string message = String.Format(UserAddingSuccess);
+                customLog.WriteEntry(message);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event (eventid = {0}) to event log.",
+                    (int)AuditEventTypes.Add));
+            }
+        }
+
+        public static void Delete()
+        {
+            if (customLog != null)
+            {
+                string UserDeleteSuccess = AuditEvents.Delete;
+                string message = String.Format(UserDeleteSuccess);
+                customLog.WriteEntry(message);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event (eventid = {0}) to event log.",
+                    (int)AuditEventTypes.Delete));
+            }
+        }
+
+        public static void Modify()
+        {
+            if (customLog != null)
+            {
+                string UserUpdatingSuccess = AuditEvents.Modify;
+                string message = String.Format(UserUpdatingSuccess);
+                customLog.WriteEntry(message);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event (eventid = {0}) to event log.",
+                    (int)AuditEventTypes.Modify));
+            }
+        }
+
+        public void Dispose()
+        {
+            if (customLog != null)
+            {
+                customLog.Dispose();
+                customLog = null;
+            }
+        }
+    }
+}
---
+++ Project_3/SecurityManager/AuditEvents.cs
@@ -0,0 +1,65 @@
+﻿using SecurityManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+	public enum AuditEventTypes
+	{
+		Add = 0,
+		Delete = 1,
+		Modify = 2,
+	}
+
+	public class AuditEvents
+	{
+		private static ResourceManager resourceManager = null;
+		private static object resourceLock = new object();
+
+		private static ResourceManager ResourceMgr
+		{
+			get
+			{
+				lock (resourceLock)
+				{
+					if (resourceManager == null)
+					{
+						resourceManager = new ResourceManager
+							(typeof(AuditEventFile).ToString(),
+							Assembly.GetExecutingAssembly());
+					}
+					return resourceManager;
+				}
+			}
+		}
+
+		public static string Add
+		{
+			get
+			{
+				return ResourceMgr.GetString(AuditEventTypes.Add.ToString());
+			}
+		}
+
+		public static string Delete
+		{
+			get
+			{
+				return ResourceMgr.GetString(AuditEventTypes.Delete.ToString());
+			}
+		}
+
+		public static string Modify
+		{
+			get
+			{
+				return ResourceMgr.GetString(AuditEventTypes.Modify.ToString());
+			}
+		}
+	}
+}
